03aac87bae61e87aded50ab133f02d6bd22e1a3a
Mohammad Shafi <umershafi02@gmail.com>
2024-05-02 16:09:57 -0700

    init

---
+++ public/app.js
@@ -0,0 +1,14 @@
+import Card from "./card.js";
+import Mover from "./mover.js";
+
+export default class App {
+  constructor() {
+    //TODO
+  }
+
+  addCard(col, title, color) {
+    //TODO
+  }
+
+  //TODO
+}
---
+++ public/card.js
@@ -0,0 +1,18 @@
+/* The text to use when description is empty */
+const NO_DESCRIPTION_TEXT = "(No description)";
+
+export default class Card {
+  constructor(title, color) {
+    //TODO
+  }
+
+  addToCol(colElem, mover) {
+    //TODO
+  }
+
+  setDescription(text) {
+    //TODO
+  }
+
+  //TODO
+}
